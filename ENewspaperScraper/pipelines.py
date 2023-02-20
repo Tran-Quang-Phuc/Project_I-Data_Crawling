1df2ac78443c5dc6061cfde6da7d0f90a28a325d
Tran-Quang-Phuc <phuc.tq204679@sis.hust.edu.vn>
2023-02-20 10:25:27 +0700

    update pipelines

--- ENewspaperScraper/pipelines.py
+++ ENewspaperScraper/pipelines.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-import gensim as gensim
-import numpy as np
 import pymongo
 from scrapy.exceptions import DropItem
 from itemadapter import ItemAdapter
-import nltk
-from nltk.tokenize import word_tokenize, sent_tokenize
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 all_messages = []
 
@@ -54,63 +54,46 @@ class ConcatenateMessagePipeline:
         message = ''
         for para in adapter.get('message'):
             message = message + para
-        adapter['message'] = message.replace('\n', '')
+        adapter['message'] = message.replace('\n', '').replace('\xa0', '')
 
         return item
 
 
-class SimilarMessageCheck:
+class SimilarityPipeline:
     def __init__(self):
-        nltk.download('punkt')
+        self.articles = set()
+
+    def _is_similar(self, article1, article2):
+        # Convert the articles to lowercase to make the comparison case-insensitive
+        article1 = article1.lower()
+        article2 = article2.lower()
+
+        # Create a TfidfVectorizer object and fit it to the articles
+        vectorizer = TfidfVectorizer()
+        vectors = vectorizer.fit_transform([article1, article2])
+
+        # Calculate the cosine similarity between the vectors
+        similarity = cosine_similarity(vectors[0], vectors[1])[0][0]
+
+        # Return True if the similarity is greater than a threshold value, False otherwise
+        return similarity > 0.8
 
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
-        cur_message = adapter.get('message')
+        message = adapter.get('message')
+        if len(message) < 100:
+            raise DropItem("This article is to short")
+        # Check if the current article is similar to any of the previously seen articles
+        for article in self.articles:
+            if self._is_similar(message, article):
+                # If the article is similar, drop it and return None
+                raise DropItem("This message has already scraped")
 
-        if len(cur_message) < 20:
-            raise DropItem("Message is too short!")
-        elif len(all_messages) != 0:
-            for message in all_messages:
-                if self.checkSimilar(cur_message, message):
-                    raise DropItem("Drop similar news!")
-                else:
-                    all_messages.append(cur_message)
-        else:
-            all_messages.append(cur_message)
+        # If the article is not similar to any of the previously seen articles, add it to the set
+        self.articles.add(message)
 
-        print("Add a new article")
-
+        # Return the item to continue processing it
         return item
-
-    def checkSimilar(self, mess1, mess2):
-        mess1_docs = []
-        for line in sent_tokenize(mess1):
-            mess1_docs.append(line)
-        gen_docs = []
-        for line in mess1_docs:
-            gen_doc = [word.lower() for word in word_tokenize(line)]
-            gen_docs.append(gen_doc)
-        dictionary = gensim.corpora.Dictionary(gen_docs)
-        corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
-        tf_idf = gensim.models.TfidfModel(corpus)
-        sims = gensim.similarities.Similarity('workdir/', tf_idf[corpus], num_features=len(dictionary))
-        mess2_docs = []
-        avg_sims = []
-        for line in sent_tokenize(mess2):
-            mess2_docs.append(line)
-        for line in mess2_docs:
-            query_doc = [w.lower() for w in word_tokenize(line)]
-            query_doc_bow = dictionary.doc2bow(query_doc)  # update an existing dictionary and create bag of words
-            query_doc_tf_idf = tf_idf[query_doc_bow]
-            sum_of_sims = np.sum(sims[query_doc_tf_idf], dtype=np.cfloat)
-            avg = sum_of_sims / len(mess1_docs)
-            avg_sims.append(avg)
-
-        total_avg = np.sum(avg_sims, dtype=np.cfloat)
-        percentage_of_similarity = np.round(float(total_avg) * 100) // 3
-        if percentage_of_similarity >= 100:
-            return True
-        return False
 
 
 class StoreToMongoPipeline:
