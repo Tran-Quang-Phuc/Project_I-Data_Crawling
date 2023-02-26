8ec8bfc8a7e0f82956a3168fb7d9e487637f3d18
Tran-Quang-Phuc <phuc.tq204679@sis.hust.edu.vn>
2023-02-27 01:29:15 +0700

    update zingnews crawler

--- ENewspaperScraper/spiders/zingnews.py
+++ ENewspaperScraper/spiders/zingnews.py
@@ -2,33 +2,34 @@ import scrapy
 
 
 class zingnewsSpider(scrapy.Spider):
-    name = 'zingnews.vn'
+    name = 'zingnews'
     allowed_domains = ['zingnews.vn']
+    custom_settings = {'CLOSESPIDER_PAGECOUNT': 100}
     start_urls = ['https://zingnews.vn/']
 
     def parse(self, response):
-        article_links = response.xpath('//p[@class="article-title"]/a/@href').getall()
-        for link in article_links:
-            response.follow(link, callback=self.parse_article)
-
         topic_links = response.xpath('//nav[@class="category-menu"]/ul/li/a/@href').getall()
         for link in topic_links:
-            response.follow(link, callback=self.parse_topic)
+            yield response.follow(link, callback=self.parse_topic)
 
-    def parse_topic(self, response):
         article_links = response.xpath('//p[@class="article-title"]/a/@href').getall()
         for link in article_links:
-            response.follow(link, callback=self.parse_article)
+            yield response.follow(link, callback=self.parse_article)
 
+    def parse_topic(self, response):
         if response.url != 'https://lifestyle.zingnews.vn/':
             cate_links = response.xpath('//section[@id="category-header"]/ul/li/a/@href').getall()
             for link in cate_links:
-                response.follow(link, callback=self.parse_category)
+                yield response.follow(link, callback=self.parse_category)
+
+        article_links = response.xpath('//p[@class="article-title"]/a/@href').getall()
+        for link in article_links:
+            yield response.follow(link, callback=self.parse_article)
 
     def parse_category(self, response):
         article_links = response.xpath('//p[@class="article-title"]/a/@href').getall()
         for link in article_links:
-            response.follow(link, callback=self.parse_article)
+            yield response.follow(link, callback=self.parse_article)
 
     def parse_article(self, response):
         pass
