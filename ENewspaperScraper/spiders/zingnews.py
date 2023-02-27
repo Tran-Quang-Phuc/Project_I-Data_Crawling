6a8bc2670f35f1a049cff4aa6fdaf7e01398323a
Tran-Quang-Phuc <phuc.tq204679@sis.hust.edu.vn>
2023-02-27 11:28:40 +0700

    update zingnews crawler

--- ENewspaperScraper/spiders/zingnews.py
+++ ENewspaperScraper/spiders/zingnews.py
@@ -1,10 +1,12 @@
 import scrapy
 
+from ENewspaperScraper.items import newsItem
+
 
 class zingnewsSpider(scrapy.Spider):
     name = 'zingnews'
     allowed_domains = ['zingnews.vn']
-    custom_settings = {'CLOSESPIDER_PAGECOUNT': 100}
+    custom_settings = {'CONCURRENT_REQUESTS': 2}
     start_urls = ['https://zingnews.vn/']
 
     def parse(self, response):
@@ -32,5 +34,36 @@ class zingnewsSpider(scrapy.Spider):
             yield response.follow(link, callback=self.parse_article)
 
     def parse_article(self, response):
-        pass
+        news = newsItem()
+
+        news['docID'] = response.xpath('//article/@article-id').get()
+        news['user'] = response.xpath('//p[@class="author"]/text()').get()
+        news['userID'] = None
+        news['type'] = response.xpath('//p[@class="the-article-category"]/a/@title').get()
+        dateString = response.xpath('//meta[@property="article:published_time"]/@content').get()
+        if dateString:
+            dateString = dateString[:-5] + '.000' + '+07:00'
+            news['createDate'] = dateString
+            news['shortFormDate'] = dateString[:10]
+        news['title'] = response.xpath('//meta[@property="og:title"]/@content').get()
+        news['description'] = response.xpath('//meta[@name="description"]/@content').get()
+        news['message'] = response.xpath('//div[@class="the-article-body"]/p//text()').getall()
+        link_selectors = response.xpath('//div[@class="inner-article"]/a')
+        news['links_in_article'] = self._getLinksInfo(link_selectors)
+        news['picture'] = response.xpath('//div[@class="the-article-body"]//img/@data-src').getall()
+
+        yield news
+
+    @staticmethod
+    def _getLinksInfo(selectors):
+        links_in_article = []
+        link = {}
+
+        for selector in selectors:
+            link['name'] = selector.xpath('./h2/text()').get()
+            link['link'] = selector.xpath('./@href').get()
+            link['description'] = selector.xpath('./p[@class="summary"]/text()').get()
+            links_in_article.append(link.copy())
+
+        return links_in_article
     
