f2b15adbde006c80e294f9c8d62048f0853abfea
Tran-Quang-Phuc <phuc.tq204679@sis.hust.edu.vn>
2023-02-27 11:30:31 +0700

    update settings

--- ENewspaperScraper/settings.py
+++ ENewspaperScraper/settings.py
@@ -21,7 +21,7 @@ NEWSPIDER_MODULE = 'ENewspaperScraper.spiders'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 25
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -51,9 +51,17 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'ENewspaperScraper.middlewares.EnewspaperscraperDownloaderMiddleware': 543,
-#}
+
+PROXY_POOL_ENABLED = True
+
+DOWNLOADER_MIDDLEWARES = {
+    # Rotate USER_AGENT
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+    # ROTATE PROXIES
+    'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
+    'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
